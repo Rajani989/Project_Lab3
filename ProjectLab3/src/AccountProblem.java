52464756f95697773442695121a54ad46a74b38f
Rajani <Rajanu@admin>
2022-09-01 18:05:18 +0530

    commit

--- ProjectLab3/src/AccountProblem.java
+++ ProjectLab3/src/AccountProblem.java
@@ -1,83 +1,98 @@
-
+/*Define class Account and it has two subclass
+CurrentAc and SavingsAc, Saving account and Current
+ Account has three method getBalance , deposit Amount,
+ Withdraw Amount(Abstract method implements by subclass), Savings account
+  has no transaction fee but current Account has transaction Fee 10 rs per transaction in Bank. */
 
 import java.util.Scanner;
 
-abstract class Account
+abstract class Account                                //abstract class account
 {
    public abstract void getBalance();
    public abstract void depositeAmount();
    public abstract void withdrawAmount();
 }
 
-class CurrentAc extends Account
+class CurrentAc extends Account                          //current account extends account
 {
 	int balance=1000,transFee=10;
-	public void getBalance()
+	public void getBalance()                               //getBalance method
 	{
-		
+		System.out.println("Balance is: "+balance);
 	}
-	public void depositeAmount(int amt)
-	{
-	balance=balance+amt-transFee;	
+	@Override
+	public void depositeAmount()                       //depositeAccount method
+	{ 
+		balance=balance+amt-transFee;	
+		System.out.println("Balance is: "+balance);
 	}
-	public void withdrawAmount(int amt)
+	@Override
+	public void withdrawAmount()                          //withdrawAccount method
 	{
-	balance=balance-amt-transFee;	
+		balance=balance-amt-transFee;	
+		System.out.println("Balance is: "+balance);
 	}
 }
 
-class SavingAc extends Account
+ class SavingAc extends Account                         //Saving Account extends Account
 {
 	int balance=1000;
-	public void getBalance()
+	public void getBalance()                              //getBalance method
 	{
-		
+		System.out.println("Balance is: "+balance);
 	}
-	public void depositeAmount(int amt)
+	@Override
+	public void depositeAmount()                       //depositeAccount method
 	{
+
 		balance=balance+amt;
+		System.out.println("Balance is: "+balance);
 	}
-	public void withdrawAmount(int amt)
+	@Override
+	public void withdrawAmount()                            //withdrawAccount method
 	{
-		balance=balance-amt;	
+		balance=balance-amt;
+		System.out.println("Balance is: "+balance);
 	}
 }
 
-public class AccountProblem {
+public class AccountProblem {                                 //class AccountProblem
 
-	public static void main(String... args) {
+	public static void main(String... args) {                  //Main method
 		// TODO Auto-generated method stub
-		Scanner sc=new Scanner(System.in);
+		Scanner sc=new Scanner(System.in);                      //Take input the details
 		System.out.println("Enter the type of Account: 1.Current Account/2.Saving Account");
 		int type=sc.nextInt();
 		System.out.println("Enter the Account number: ");
 		int num=sc.nextInt();
-        System.out.println("what you want 3.deposite/4.withdraw: ");
+        System.out.println("what you want : ");
+        System.out.println("Press number: 3.Deposite 4.Withdraw 5.GetBalance"); 
         int val=sc.nextInt();
         System.out.println("Enter the amount: ");
         int amt=sc.nextInt();
-         System.out.println("Press number: 3.Deposite 4.Withdraw 5.GetBalance") 
-        Account ca=new CurrentAc();
-         Account sa=new SavingAc();
+      
+         
+        Account ca=new CurrentAc();              //call the method
+         Account sa=new SavingAc();              //call the method
         
-        switch(type)
+        switch(type)                  //switch statements to call and print
         {
         case 1: 
         	    switch(val)
         	    {
-        	    case 3: ca.depositeAmount(amt);
+        	    case 3: ca.depositeAmount();
         	    break;
         	    case 4: ca.withdrawAmount(amt);
         	    break;
-        	    case 5: ca.getBalance();
+        	    case 5: ca.getBalance(amt);
         	    
         	    }
         case 2:
         	 switch(val)
      	    {
-     	    case 3: ca.depositeAmount(amt);
+     	    case 3: ca.depositeAmount();
      	    break;
-     	    case 4: ca.withdrawAmount(amt);
+     	    case 4: ca.withdrawAmount();
      	    break;
      	    case 5: ca.getBalance();
      	    }
